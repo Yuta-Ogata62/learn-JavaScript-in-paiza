7a64c09232091929cf238f76a053c2a102332b08
Yuta-Ogata62 <yuta.ogata62@gmail.com>
2023-03-21 18:17:08 +0900

    janken

---
+++ Rock_Paper_Scissors.js
@@ -0,0 +1,10 @@
+const values = ["グー", "チョキ", "パー"]
+console.log(values);
+
+let janken = values.length;
+
+    var hands = Math.floor(Math.random() * janken);
+    //ランダム
+    console.log(values[hands]);
+
+    // 選ばれた手を出力する
